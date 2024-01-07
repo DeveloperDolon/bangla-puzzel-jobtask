1f0e34df705f0a2b5cb2469d31dee5e462618775
Dolon Roy <dolonr718@gmail.com>
2024-01-07 23:08:38 +0600

    add to cart functionality added commit

--- src/components/ProductCards/ProductsCards.jsx
+++ src/components/ProductCards/ProductsCards.jsx
@@ -8,36 +8,11 @@ import { useContext } from "react";
 import { AuthContext } from "../../contextApi/AuthProvider";
 
 const ProductsCards = () => {
-    const {setCart} = useContext(AuthContext);
+    const {cartIds, setCartIds, foodsItems} = useContext(AuthContext);
 
     const handleAddToCart = (item) => {
-        item.quantity = 1;
-        console.log(item);
+        setCartIds([...cartIds, item]);
     }
-
-    const foodsItems = [
-        {
-            "id": "1",
-            "name": "Chicken Fry",
-            "image": "https://images.unsplash.com/photo-1626082896492-766af4eb6501?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZnJpZWQlMjBjaGlja2VufGVufDB8fDB8fHww",
-            "price": 50,
-            "description": "Crispy, golden chicken fry with a flavorful blend of spices,tender on the inside and irresistibly delicious -a perfect balance of crunch and taste."
-        },
-        {
-            "id": "2",
-            "name": "Chicken fajitas",
-            "image": "https://media.istockphoto.com/id/507532058/photo/mexican-grilled-chicken-fajitas-in-iron-skillet.jpg?s=612x612&w=0&k=20&c=kiYNoqQFc3-qAVV4VYYc0qSKKExzCBBf1qv4a1X-BH4=",
-            "price": 250,
-            "description": "Sizzling chicken fajitas: Juicy grilled chicken, vibrant bell peppers, and onions, wrapped in warm tortillas. A zesty fiesta of flavors in every bite!"
-        },
-        {
-            "id": "3",
-            "name": "Chicken Masala",
-            "image": "https://media.istockphoto.com/id/1093661590/photo/traditional-indian-dish-chicken-tikka-masala-with-spicy-curry-meat-in-bowl-basmati-rice-bread.jpg?s=612x612&w=0&k=20&c=DCwxqEciOTaN14A13ym6F4p2UnprhNarn7OwIaOLNNo=",
-            "price": 450,
-            "description": "Savory chicken masala:Succulent pieces in a rich,aromatic blend of spices and tomatoes.Indian flavors that tantalize the taste buds."
-        }
-    ]
 
     return (
         <Container maxWidth="lg">
@@ -66,8 +41,11 @@ const ProductsCards = () => {
                         </CardContent>
 
                         <div className="p-4 space-y-4">
-                            <Button onClick={() => handleAddToCart(item)} variant="contained" sx={{width: "100%", background: "#fd5442"}}>Add To Order</Button>
-                            <Button variant="outlined" sx={{width: "100%", color: "#fd5442", border: "2px solid #fd5442", fontWeight: "600"}}>Customize</Button>
+                            {
+                                cartIds?.includes(item?.id) ? <Button disabled variant="contained" sx={{width: "100%", background: "#fd5442"}}>Added To Cart</Button> : <Button onClick={() => handleAddToCart(item?.id)} variant="contained" sx={{width: "100%", background: "#fd5442"}}>Add To Order</Button>
+                            }
+                            
+                            <Button variant="outlined" sx={{width: "100%", color: "#fd5442", border: "2px solid #fd5442", fontWeight: "600"}}>Customize {item?.id}</Button>
                         </div>
                     </Card>)
                 }
--- src/contextApi/AuthProvider.jsx
+++ src/contextApi/AuthProvider.jsx
@@ -1,14 +1,51 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
 export const AuthContext = createContext();
 
 const AuthProvider = ({children}) => {
     const [cart, setCart] = useState([]); 
+    const [cartIds, setCartIds] = useState([]);
+    const foodsItems = [
+        {
+            "id": "1",
+            "name": "Chicken Fry",
+            "image": "https://images.unsplash.com/photo-1626082896492-766af4eb6501?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZnJpZWQlMjBjaGlja2VufGVufDB8fDB8fHww",
+            "price": 50,
+            "description": "Crispy, golden chicken fry with a flavorful blend of spices,tender on the inside and irresistibly delicious -a perfect balance of crunch and taste."
+        },
+        {
+            "id": "2",
+            "name": "Chicken fajitas",
+            "image": "https://media.istockphoto.com/id/507532058/photo/mexican-grilled-chicken-fajitas-in-iron-skillet.jpg?s=612x612&w=0&k=20&c=kiYNoqQFc3-qAVV4VYYc0qSKKExzCBBf1qv4a1X-BH4=",
+            "price": 250,
+            "description": "Sizzling chicken fajitas: Juicy grilled chicken, vibrant bell peppers, and onions, wrapped in warm tortillas. A zesty fiesta of flavors in every bite!"
+        },
+        {
+            "id": "3",
+            "name": "Chicken Masala",
+            "image": "https://media.istockphoto.com/id/1093661590/photo/traditional-indian-dish-chicken-tikka-masala-with-spicy-curry-meat-in-bowl-basmati-rice-bread.jpg?s=612x612&w=0&k=20&c=DCwxqEciOTaN14A13ym6F4p2UnprhNarn7OwIaOLNNo=",
+            "price": 450,
+            "description": "Savory chicken masala:Succulent pieces in a rich,aromatic blend of spices and tomatoes.Indian flavors that tantalize the taste buds."
+        }
+    ]
+
+    useEffect(() => {
+        const cartItems = foodsItems.filter(item => {
+            if(cartIds.includes(item?.id)) {
+                return item;
+            }
+        });
+
+        setCart(cartItems);
+    }, [cartIds]);
 
     const sharedData = {
         cart,
-        setCart
+        setCart,
+        cartIds,
+        setCartIds,
+        foodsItems
     }
 
 
